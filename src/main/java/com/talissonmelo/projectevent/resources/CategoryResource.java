bb3a33b9605cc9f1fb3378d5741cd1797f82486e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-02-15 16:44:58 -0300

    Testando classe de dominio - Categoria

--- src/main/java/com/talissonmelo/projectevent/resources/CategoryResource.java
+++ src/main/java/com/talissonmelo/projectevent/resources/CategoryResource.java
@@ -1,15 +1,30 @@
 package com.talissonmelo.projectevent.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.talissonmelo.projectevent.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String test() {
-		return "REST está funcionando";
+	public List<Category> findAll(){
+		
+		Category cat1 = new Category(1, "Entretenimento");
+		Category cat2 = new Category(2, "Corporativos");
+		
+		List<Category> categories = new ArrayList<>();
+		categories.add(cat1);
+		categories.add(cat2);
+		
+		return categories;
+		
 	}
+	
 }
