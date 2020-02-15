8cfea0b5904b2a9dab1af8d6f2702aa98efdb0af
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-02-15 16:13:26 -0300

    Projeto criado

---
+++ src/main/java/com/talissonmelo/projectevent/ProjectEventApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo.projectevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEventApplication.class, args);
+	}
+
+}
