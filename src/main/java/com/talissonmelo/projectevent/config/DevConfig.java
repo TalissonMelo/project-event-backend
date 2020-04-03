5ff1267ad38609937874e67a4fdc5bb6159995b2
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-04-03 08:45:01 -0300

    Profile Dev

--- src/main/java/com/talissonmelo/projectevent/config/DevConfig.java
+++ src/main/java/com/talissonmelo/projectevent/config/DevConfig.java
@@ -3,29 +3,36 @@ package com.talissonmelo.projectevent.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.talissonmelo.projectevent.services.DBService;
 import com.talissonmelo.projectevent.services.EmailService;
-import com.talissonmelo.projectevent.services.MockEmailService;
+import com.talissonmelo.projectevent.services.SmtpEmailService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
-	
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
 		dbService.instantiateTestDataBase();
 		return true;
 	}
-	
+
 	@Bean
 	public EmailService emailService() {
-		return new MockEmailService();
+		return new SmtpEmailService();
 	}
 }
