69b8fdf935d0f26e78192c3c8d9a6a4da0db0712
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-06-18 20:15:01 -0300

    Configurando CORS

--- src/main/java/com/talissonmelo/projectevent/ProjectEventApplication.java
+++ src/main/java/com/talissonmelo/projectevent/ProjectEventApplication.java
@@ -1,28 +1,14 @@
 package com.talissonmelo.projectevent;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class ProjectEventApplication implements CommandLineRunner, WebMvcConfigurer {
+public class ProjectEventApplication implements WebMvcConfigurer {
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectEventApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 	}
 
 }
---
+++ src/main/java/com/talissonmelo/projectevent/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.talissonmelo.projectevent.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+	}
+
+}
