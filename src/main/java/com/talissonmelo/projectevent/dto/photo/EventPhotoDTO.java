c71d8cac7d83518d48b7d1ace8097ccd02b18794
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-07-24 09:51:52 -0300

    Arquivos aceitos nos formatos PDF, JPEG e PNG

--- src/main/java/com/talissonmelo/projectevent/dto/photo/EventPhotoDTO.java
+++ src/main/java/com/talissonmelo/projectevent/dto/photo/EventPhotoDTO.java
@@ -3,14 +3,17 @@ package com.talissonmelo.projectevent.dto.photo;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.talissonmelo.projectevent.config.validation.FileContentType;
 import com.talissonmelo.projectevent.config.validation.FileSize;
 
 public class EventPhotoDTO {
 
 	@NotNull
 	@FileSize(max = "800KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.APPLICATION_PDF_VALUE })
 	private MultipartFile file;
 	
 	@NotBlank
