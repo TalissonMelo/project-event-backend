b54f686d5eab125aa4fdd3280b378a18706a196e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-07-24 19:28:40 -0300

    Servindo arquivos de fotos pela API

--- src/main/java/com/talissonmelo/projectevent/resources/EventoPhotoResource.java
+++ src/main/java/com/talissonmelo/projectevent/resources/EventoPhotoResource.java
@@ -1,11 +1,15 @@
 package com.talissonmelo.projectevent.resources;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +22,7 @@ import com.talissonmelo.projectevent.domain.Photo;
 import com.talissonmelo.projectevent.dto.EventPhotoDTO;
 import com.talissonmelo.projectevent.services.EventPhotoService;
 import com.talissonmelo.projectevent.services.EventService;
+import com.talissonmelo.projectevent.services.storage.PhotoStorageService;
 
 @RestController
 @RequestMapping(value = "events/{eventId}/photo")
@@ -25,6 +30,9 @@ public class EventoPhotoResource {
 	
 	@Autowired
 	private EventService eventService;
+	
+	@Autowired
+	private PhotoStorageService photoStorageService;
 
 	@Autowired
 	private EventPhotoService eventPhotoService;
@@ -48,5 +56,17 @@ public class EventoPhotoResource {
 	@GetMapping
 	public Photo findByPhotoEvent(@PathVariable Integer eventId) {
 		return eventPhotoService.findByPhoto(eventId);
+	}
+	
+	@GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<InputStreamResource> outputPhoto(@PathVariable Integer eventId) {
+
+		try {
+			Photo photo = eventPhotoService.findByPhoto(eventId);
+			InputStream inputStream = photoStorageService.findByPhoto(photo.getName());
+			return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(new InputStreamResource(inputStream));
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
