9953e014371e5ba23de21859c1a79240c96c7275
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-21 06:50:27 -0300

    Descrevendo informações da API na documentação

--- src/main/java/com/talissonmelo/projectevent/config/SpringDocumentation.java
+++ src/main/java/com/talissonmelo/projectevent/config/SpringDocumentation.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,13 +19,22 @@ public class SpringDocumentation implements WebMvcConfigurer {
 
 	@Bean
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.talissonmelo.projectevent.resources")).build();
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.talissonmelo.projectevent.resources")).build().apiInfo(apiInfo());
 	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+					.title("Event")
+					.description("API aberta para estudantes e apaixonados por programação")
+					.version("1")
+					.contact(new Contact("Talisson De Melo Rodrigues", "https://projeto-integrador-projeto-implementacao-event.netlify.app/", "talisson.cursos@gmail.com"))
+					.build();
 	}
 
 }
