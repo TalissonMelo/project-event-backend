dbc42a21ace9e1a687e5c8d77ec750dc99781462
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-09 18:47:08 -0300

    Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/talissonmelo/projectevent/config/SpringDocumentation.java
+++ src/main/java/com/talissonmelo/projectevent/config/SpringDocumentation.java
@@ -1,14 +1,21 @@
 package com.talissonmelo.projectevent.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,8 +32,24 @@ public class SpringDocumentation implements WebMvcConfigurer {
 				.apis(RequestHandlerSelectors
 				.basePackage("com.talissonmelo.projectevent.resources"))
 				.build()
+				.useDefaultResponseMessages(true)
+				.globalResponseMessage(RequestMethod.GET,  globalResponseMessage())
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"));
+	}
+	
+	private List<ResponseMessage> globalResponseMessage(){
+		return Arrays.asList(
+				new ResponseMessageBuilder()
+				.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+				.message("Erro interno do servidor")
+				.build(),
+			
+				new ResponseMessageBuilder()
+				.code(HttpStatus.NOT_ACCEPTABLE.value())
+				.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+				.build()
+		);
 	}
 
 	@Override
