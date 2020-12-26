32cd37d8746d2652c4c7fa2d5dab49c5df801b10
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-26 09:27:00 -0300

    Criando o projeto do Authorization Server com Spring Security OAuth2

---
+++ event-auth/src/main/java/com/talissonmelo/EventAuthApplication.java
@@ -0,0 +1,13 @@
+package com.talissonmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventAuthApplication.class, args);
+	}
+
+}
