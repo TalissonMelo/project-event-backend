7585ed29ee3dc385733165ca22f8916922bee052
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-01-06 19:25:32 -0300

    Configurando o Refresh Token Grant Type no Authorization Server

--- event-auth/src/main/java/com/talissonmelo/config/AuthorizationServerConfig.java
+++ event-auth/src/main/java/com/talissonmelo/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.talissonmelo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -19,6 +20,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailsService userService;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -26,7 +30,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.inMemory()
 				.withClient("event-web")
 				.secret(encoder.encode("web123"))
-				.authorizedGrantTypes("password")
+				.authorizedGrantTypes("password", "refresh_token")
 				.scopes("write","read")
 				.accessTokenValiditySeconds(60*60*6)
 			.and()
@@ -44,6 +48,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints
+		.authenticationManager(authenticationManager)
+		.userDetailsService(userService);
 	}
 }
--- event-auth/src/main/java/com/talissonmelo/security/WebSecurityConfig.java
+++ event-auth/src/main/java/com/talissonmelo/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -34,7 +35,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	@Override
 	protected AuthenticationManager authenticationManager() throws Exception {
-		// TODO Auto-generated method stub
 		return super.authenticationManager();
+	}
+	
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		return super.userDetailsService();
 	}
 }
